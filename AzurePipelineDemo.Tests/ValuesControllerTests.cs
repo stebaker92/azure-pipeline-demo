d81744e94bfff2012ff7f53eac4f9bbde53c8151
Steve Baker <ste.baker.92@gmail.com>
2019-01-11 15:21:05 +0000

    Added test project

---
+++ AzurePipelineDemo.Tests/ValuesControllerTests.cs
@@ -0,0 +1,27 @@
+using azure_pipeline_demo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AzurePipelineDemo.Tests
+{
+    public class ValuesControllerTests
+    {
+        private readonly ValuesController controller;
+
+        public ValuesControllerTests()
+        {
+            controller = new ValuesController();
+        }
+
+        [Fact]
+        public void TestGetResults()
+        {
+            var result = controller.Get();
+
+            Assert.IsType<ActionResult<IEnumerable<string>>>(result);
+        }
+    }
+}
